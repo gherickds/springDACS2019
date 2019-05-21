05ca1796c5c67bb391952ced3ada4cdc736cde95
ERICK DE SOUZA <erick.souza@univille.br>
2019-05-21 20:09:48 -0300

    first version

---
+++ src/main/java/br/univille/dacs2019/ErickDacs2019Application.java
@@ -0,0 +1,13 @@
+package br.univille.dacs2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErickDacs2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErickDacs2019Application.class, args);
+	}
+
+}
---
+++ src/main/java/br/univille/dacs2019/service/ClienteService.java
@@ -0,0 +1,12 @@
+package br.univille.dacs2019.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import br.univille.dacs2019.model.Cliente;
+
+@Service
+public interface ClienteService {
+
+	List<Cliente> getAll();
+}
